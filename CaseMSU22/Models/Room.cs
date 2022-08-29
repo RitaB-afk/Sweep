cd1493ee42d72fd5c8d1c7730b028efe83cd4caf
RitaB <77437092+RitaB-afk@users.noreply.github.com>
2022-08-29 12:24:16 +0200

    Create project and class
    created project and class

---
+++ CaseMSU22/Models/Room.cs
@@ -0,0 +1,13 @@
+﻿namespace CaseMSU22.Models
+{
+    public class Room
+    {
+        public int RoomNumber { get; set; }
+        public string RoomCategory { get; set; }
+        public int RoomLevel { get; set; }
+        public string RoomOccupancyStatus { get; set; }
+        public DateTime LastCleaned { get; set; }
+        public string CleaningStatus { get; set; }
+        public string Comments { get; set; }
+    }
+}
